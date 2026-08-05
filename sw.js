9a56c2b0a7db4d7390bacd9d648d69f26c9fda68
axeleyyachnemev <a.yachmenev@gmail.com>
2026-08-05 16:48:41 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Офлайн-кэш. Меняй VERSION при обновлении index.html — иначе телефон покажет старую копию. */
-var VERSION = 'faza0-v3';
+var VERSION = 'faza0-v4';
 var FILES = [
   './',
   './index.html',
